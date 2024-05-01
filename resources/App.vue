63cd63652aa1b073cbcf0f90cac80dd791aa25b7
Dhaverd <jedi55@mail.ru>
2024-05-01 15:03:39 +0800

    initial-commit

---
+++ resources/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-app>
+        <SomeTemplate></SomeTemplate>
+    </v-app>
+</template>
+
+<script>
+import SomeTemplate from "./views/SomeTemplate.vue";
+
+export default {
+    name: "App",
+    components: {SomeTemplate}
+}
+</script>
+
+
